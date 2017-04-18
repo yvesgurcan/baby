a723a2e2704bb3a164adf55965a53b8c6d9aaec9
gerkhin <gurcan.yves@gmail.com>
2017-04-17 22:26:28 -0700

    created user form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, {Component} from 'react';
-import {Button, FormControl, FormGroup, Col, Alert} from 'react-bootstrap'
+import {Button, FormControl, FormGroup, Col, Alert, Grid, Row, Checkbox} from 'react-bootstrap'
 import './App.css';
 
 class Flags extends Component {
   render() {
-    console.log("test")
     return (
       <div>flags</div>
     )
@@ -36,17 +35,189 @@ class Questions extends Component {
 }
 
 class CreateProfile extends Component {
+  daysOfTheMonth() {
+    let days = []
+    const totalDays = new Date(this.props.year, this.props.month, 0).getDate()
+    console.log(totalDays)
+    for (var i = 1; i <= totalDays; i++) {
+      days.push(<option value={i}>{i}</option>)
+    }
+    return days
+  }
+  months() {
+    const listOfMonths = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+    let months = listOfMonths.map((month, index) => {
+      return <option value={index+1}>{month}</option>
+    })
+    return months
+  }
+  years() {
+    let years = []
+    for (var i = 1935; i <= 2010; i++) {
+      years.push(<option value={i}>{i}</option>)
+    }
+    return years
+  }
+  ageCategories() {
+    const categories = [{id:"24+",name:"24 -"},{id:"25-49",name:"25-49"},{id:"50-74",name:"50-74"},{id:"75+",name:"75+"}]
+    let ageCategories = categories.map((category) => {
+      return <option value={category.id}>{category.name}</option>
+    })
+    return ageCategories
+  }
+  residences() {
+    const countries = [{id:"france",name:"France"},{id:"usa",name:"United States"}]
+    let residence = countries.map((country) => {
+      return <option value={country.id}>{country.name}</option>
+    })
+    return residence
+  }
+  relationships() {
+    const relations = [{id: "relative", name:"Relative"},{id:"friend",name:"Friend"},{id:"coworker",name:"Coworker"}]
+    let relationships = relations.map((relation) => {
+      return <option value={relation.id}>{relation.name}</option>
+    })
+    return relationships
+  }
   render() {
     return (
-      <Col xs={12}>
-        create profile
+      <div>
+        <Col sm={2}>
+          <FormControl
+            name="gender"
+            value={this.props.gender}
+            componentClass="select"
+            bsSize="large"
+            className="margin-bottom"
+            onChange={this.props.storeData}
+          >
+            <option value="F">F</option>
+            <option value="M">M</option>
+          </FormControl>
+        </Col>
+        <Col sm={5}>
+          <FormControl
+            name="name"
+            value={this.props.name}
+            bsSize="large"
+            className="margin-bottom"
+            onChange={this.props.storeData}
+          />
+          {
+            !this.props.errors.name ?
+            null :
+            <Alert
+            bsStyle="danger">
+            {this.props.errors.name}
+            </Alert>
+          }
+        </Col>
+        <Col sm={5}>
+          <FormControl
+            disabled={true}
+            value={this.props.email}
+            bsSize="large"
+            className="margin-bottom"
+          />
+        </Col>
+        <Row>
+          {!this.props.hideBirthday ? <Col sm={11}>
+            <Col smOffset={2} sm={2}>
+              <FormControl
+                name="day"
+                value={this.props.day}
+                componentClass="select"
+                bsSize="large"
+                className="margin-bottom"
+                onChange={this.props.storeData}
+              >
+                {this.daysOfTheMonth()}
+              </FormControl>
+            </Col>
+            <Col sm={4}>
+              <FormControl
+                name="month"
+                value={this.props.month}
+                componentClass="select"
+                bsSize="large"
+                className="margin-bottom"
+                onChange={this.props.storeData}
+              >
+                {this.months()}
+              </FormControl>
+            </Col>
+            <Col sm={3}>
+              <FormControl
+                name="year"
+                value={this.props.year}
+                componentClass="select"
+                bsSize="large"
+                className="margin-bottom"
+                onChange={this.props.storeData}
+              >
+                {this.years()}
+              </FormControl>
+            </Col>
+          </Col>
+          :
+          <Col sm={11}>
+            <Col smOffset={3} sm={4}>
+              <FormControl
+                name="age-category"
+                value={this.props["age-category"]}
+                componentClass="select"
+                bsSize="large"
+                className="margin-bottom"
+                onChange={this.props.storeData}
+              >
+                {this.ageCategories()}
+              </FormControl>
+              </Col>
+          </Col>
+          }
+        </Row>
+        <Row className="text-center">
+            <Col>
+              <Checkbox
+                name="hideBirthday"
+                value={this.props.hideBirthday}
+                bsSize="large"
+                className="margin-bottom"
+                onChange={this.props.storeData}
+              />      
+            </Col>        
+        </Row>
+        <Col sm={6}>
+          <FormControl
+            name="residence"
+            value={this.props.residence}
+            componentClass="select"
+            bsSize="large"
+            className="margin-bottom"
+            onChange={this.props.storeData}
+          >
+            {this.residences()}
+          </FormControl>
+        </Col>
+        <Col sm={6}>
+          <FormControl
+            name="relationship"
+            value={this.props.relationship}
+            componentClass="select"
+            bsSize="large"
+            className="margin-bottom"
+            onChange={this.props.storeData}
+          >
+            {this.relationships()}
+          </FormControl>
+        </Col>
         <Button
           block
           bsSize="large"
           bsStyle="primary"
           onClick={this.props.submitProfile}
         >Create Profile</Button>
-      </Col>
+      </div>
     )
   }
 }
@@ -68,24 +239,18 @@ class Announcement extends Component {
 }
 
 class Login extends Component {
-  constructor (props) {
-    super(props)
-    this.storeLoginData = this.storeLoginData.bind(this)
-  }
-  storeLoginData(input) {
-    this.props.storeLoginData(input)
-  }
   render() {
     return (
-      <div>
-        <Col lg={4} md={3} sm={3} xs={2}/>
-        <Col lg={4} md={6} sm={6} xs={8}>
+        <Col
+          lgOffset={2} mdOffset={3} smOffset={2} xsOffset={1}
+          lg={8} md={6} sm={8} xs={10}
+          >
           <FormControl
             name="user"
             type="email"
             bsSize="large"
             className="margin-bottom"
-            onChange={this.storeLoginData}
+            onChange={this.props.storeData}
           />
           {
             !this.props.errors.user ?
@@ -100,7 +265,7 @@ class Login extends Component {
             type="password"
             bsSize="large"
             className="margin-bottom"
-            onChange={this.storeLoginData}
+            onChange={this.props.storeData}
           />
           {
             !this.props.errors.password ?
@@ -118,8 +283,6 @@ class Login extends Component {
             onClick={this.props.submitLogin}
           >Enter</Button>
         </Col>
-        <Col lg={4} md={3} sm={3} xs={2}/>
-      </div>
     )
   }
 }
@@ -138,27 +301,27 @@ class PageSelector extends Component {
     super(props)
     // state
     this.state = {
-      currentPage: "login",
+      currentPage: "createProfile",
       user: "",
       password: "",
+      gender: "F",
+      name: "",
+      day: 1,
+      month: 1,
+      year: 1960,
+      ["age-category"]: "50-74",
+      relationship: "relative",
+      residence: "france",
       errors: {},
     }
     // functions
-    this.storeLoginData = this.storeLoginData.bind(this)
+    this.storeData = this.storeData.bind(this)
     this.submitLogin = this.submitLogin.bind(this)
     this.goToNewProfilePage = this.goToNewProfilePage.bind(this)
-    this.storeProfileData = this.storeProfileData.bind(this)
     this.submitProfile = this.submitProfile.bind(this)
-    this.storeAnswersData = this.storeAnswersData.bind(this)
     this.submitAnswers = this.submitAnswers.bind(this)
     this.fetchSurvey = this.fetchSurvey.bind(this)
     this.api = this.api.bind(this)
-  }
-  storeLoginData(input) {
-    let state = this.state
-    // console.log(input.target)
-    state[input.target.name] = input.target.value
-    this.setState(state)
   }
   submitLogin() {
     let error = false
@@ -200,12 +363,16 @@ class PageSelector extends Component {
   goToNewProfilePage() {
     this.setState({currentPage: "createProfile"})
   }
-  storeProfileData() {
-
-  }
   submitProfile() {
     let error = false
     let errorMessages = this.state.errors
+
+    // validates user's email
+    if (this.state.name.length === 0) {
+      error = true
+      errorMessages["name"] = "Please enter your name."
+    }
+
     if (error) {
       this.setState(errorMessages)
     }
@@ -216,9 +383,6 @@ class PageSelector extends Component {
         {}
       ) 
     }
-  }
-  storeAnswersData() {
-
   }
   submitAnswers() {
 
@@ -232,6 +396,17 @@ class PageSelector extends Component {
     console.log(response)
     this.setState(response)
   }
+  storeData(input) {
+    let state = this.state
+    console.log(input.target)
+    if (input.target.type === "checkbox") {
+      state[input.target.name] = input.target.checked
+    }
+    else {
+      state[input.target.name] = input.target.value
+    }
+    this.setState(state)
+  }
   render() {
     // chooses appropriate page to show
     let showFlags = true
@@ -244,7 +419,7 @@ class PageSelector extends Component {
       case "login":
         currentPage = (
           <Login
-            storeLoginData={this.storeLoginData}
+            storeData={this.storeData}
             submitLogin={this.submitLogin}
             errors={this.state.errors}
           />
@@ -261,14 +436,25 @@ class PageSelector extends Component {
       case "createProfile":
         currentPage = (
           <CreateProfile
-            storeProfileData={this.storeProfileData}
+            storeData={this.storeData}
             submitProfile={this.submitProfile}
+            errors={this.state.errors}
+            name={this.state.name}
+            email={this.state.user}
+            day={this.state.day}
+            month={this.state.month}
+            year={this.state.year}
+            hideBirthday={this.state.hideBirthday}
+            ageCategory={this.state.ageCategory}
+            residence={this.state.residence}
+            relationship={this.state.relationship}
           />
         )
         break
       case "questions":
         currentPage = (
           <Questions
+            storeData={this.storeData}
           />
         )
         break
@@ -280,11 +466,14 @@ class PageSelector extends Component {
         break
     }
     return (
-      <div>
+      <Col
+        lgOffset={3} mdOffset={2} smOffset={1}
+        lg={6} md={8} sm={10}
+      >
         <Header/>
         {currentPage}
         {showFlags ? <Flags/> : null}
-      </div>
+      </Col>
     )
   }
 }
